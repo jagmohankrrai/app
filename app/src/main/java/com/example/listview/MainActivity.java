4583f8aa52e9f0807bf3fcbf93e8591cbf6376b3
jagmohankrrai <jagmohankrrai@gmail.com>
2022-10-07 19:25:19 +0530

    https://github.com/jagmohankrrai/app.git

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView mylistview=findViewById(R.id.list);
+
+    }
+}
