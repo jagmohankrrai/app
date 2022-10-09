8368b717a50e48156a7feeab576cdb44c0990d54
jagmohankrrai <jagmohankrrai@gmail.com>
2022-10-09 13:33:18 +0530

    https://github.com/jagmohankrrai/app.git

--- app/src/main/java/com/example/listview/admin_screen.java
+++ app/src/main/java/com/example/listview/admin_screen.java
@@ -2,7 +2,9 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class admin_screen extends AppCompatActivity {
@@ -12,5 +14,11 @@ public class admin_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_screen);
         maddmem=findViewById(R.id.button);
+        maddmem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(admin_screen.this, add_employee.class));
+            }
+        });
     }
 }
